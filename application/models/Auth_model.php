12d7aa969d394a4d295c992b1e5359d585ec7aa3
Jonathan Yombo <jonathanyombo@gmail.com>
2019-09-10 21:26:29 +0200

    [NEW] model for authentification
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

---
+++ application/models/Auth_model.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth_model extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function login($username, $password)
+    {
+        $this->db->where('username', $username);
+        $this->db->or_where('email', $username);
+        $this->db->where('password', $password);
+        return $this->db->get('user')->row_array();
+    }
+}
