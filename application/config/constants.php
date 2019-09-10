874f6fee478162f85c7b3c11a03cdeae9cedbca6
Jonathan Yombo <jonathanyombo@gmail.com>
2019-09-10 09:54:21 +0200

    [UPDATE] add CODERS constant
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,3 +85,4 @@ defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 defined('APP_NAME') OR define('APP_NAME', 'forcast firme');
+defined('CODERS') OR define('CODERS', 'Carole & Sarah');
