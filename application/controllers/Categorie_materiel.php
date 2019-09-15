9ed7117a6b1adb01dc2648d37bdc9d35fb84981b
Jonathan Yombo <jonathanyombo@gmail.com>
2019-09-15 23:33:40 +0200

    [NEW] add categorie_materiel controller
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

---
+++ application/controllers/Categorie_materiel.php
@@ -0,0 +1,216 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Categorie_materiel extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Categorie_materiel_model');
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        $q = urldecode($this->input->get('q', TRUE));
+        $start = intval($this->input->get('start'));
+        
+        if ($q <> '') {
+            $config['base_url'] = base_url() . 'categorie_materiel/index.html?q=' . urlencode($q);
+            $config['first_url'] = base_url() . 'categorie_materiel/index.html?q=' . urlencode($q);
+        } else {
+            $config['base_url'] = base_url() . 'categorie_materiel/index.html';
+            $config['first_url'] = base_url() . 'categorie_materiel/index.html';
+        }
+
+        $config['per_page'] = 10;
+        $config['page_query_string'] = TRUE;
+        $config['total_rows'] = $this->Categorie_materiel_model->total_rows($q);
+        $categorie_materiel = $this->Categorie_materiel_model->get_limit_data($config['per_page'], $start, $q);
+
+        $this->load->library('pagination');
+        $this->pagination->initialize($config);
+
+        $data = array(
+            'categorie_materiel_data' => $categorie_materiel,
+            'q' => $q,
+            'pagination' => $this->pagination->create_links(),
+            'total_rows' => $config['total_rows'],
+            'start' => $start,
+        );
+        $data['page'] = $this->load->view('categorie_materiel/categorie_materiel_list', $data, TRUE);
+        $this->load->view('layouts/main', $data, FALSE);
+    }
+
+    public function read($id) 
+    {
+        $row = $this->Categorie_materiel_model->get_by_id($id);
+        if ($row) {
+            $data = array(
+		'id_cat_mat' => $row->id_cat_mat,
+		'nom_cat_mat' => $row->nom_cat_mat,
+		'date_creation_cat' => $row->date_creation_cat,
+		'details_cat_ma' => $row->details_cat_ma,
+		'etat_cat_mat' => $row->etat_cat_mat,
+	    );
+            $data['page'] = $this->load->view('categorie_materiel/categorie_materiel_read', $data, TRUE);
+            $this->load->view('layouts/main', $data, FALSE);
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('categorie_materiel'));
+        }
+    }
+
+    public function create() 
+    {
+        $data = array(
+            'button' => 'Create',
+            'action' => site_url('categorie_materiel/create_action'),
+	    'id_cat_mat' => set_value('id_cat_mat'),
+	    'nom_cat_mat' => set_value('nom_cat_mat'),
+	    'date_creation_cat' => set_value('date_creation_cat'),
+	    'details_cat_ma' => set_value('details_cat_ma'),
+	    'etat_cat_mat' => set_value('etat_cat_mat'),
+	);
+        $data['page'] = $this->load->view('categorie_materiel/categorie_materiel_form', $data, TRUE);
+        $this->load->view('layouts/main', $data, FALSE);
+    }
+    
+    public function create_action() 
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->create();
+        } else {
+            $data = array(
+		'nom_cat_mat' => $this->input->post('nom_cat_mat',TRUE),
+		'date_creation_cat' => $this->input->post('date_creation_cat',TRUE),
+		'details_cat_ma' => $this->input->post('details_cat_ma',TRUE),
+		'etat_cat_mat' => $this->input->post('etat_cat_mat',TRUE),
+	    );
+
+            $this->Categorie_materiel_model->insert($data);
+            $this->session->set_flashdata('message', 'Create Record Success');
+            redirect(site_url('categorie_materiel'));
+        }
+    }
+    
+    public function update($id) 
+    {
+        $row = $this->Categorie_materiel_model->get_by_id($id);
+
+        if ($row) {
+            $data = array(
+                'button' => 'Update',
+                'action' => site_url('categorie_materiel/update_action'),
+		'id_cat_mat' => set_value('id_cat_mat', $row->id_cat_mat),
+		'nom_cat_mat' => set_value('nom_cat_mat', $row->nom_cat_mat),
+		'date_creation_cat' => set_value('date_creation_cat', $row->date_creation_cat),
+		'details_cat_ma' => set_value('details_cat_ma', $row->details_cat_ma),
+		'etat_cat_mat' => set_value('etat_cat_mat', $row->etat_cat_mat),
+	    );
+            $data['page'] = $this->load->view('categorie_materiel/categorie_materiel_form', $data, TRUE);
+            $this->load->view('layouts/main', $data, FALSE);
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('categorie_materiel'));
+        }
+    }
+    
+    public function update_action() 
+    {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->update($this->input->post('id_cat_mat', TRUE));
+        } else {
+            $data = array(
+		'nom_cat_mat' => $this->input->post('nom_cat_mat',TRUE),
+		'date_creation_cat' => $this->input->post('date_creation_cat',TRUE),
+		'details_cat_ma' => $this->input->post('details_cat_ma',TRUE),
+		'etat_cat_mat' => $this->input->post('etat_cat_mat',TRUE),
+	    );
+
+            $this->Categorie_materiel_model->update($this->input->post('id_cat_mat', TRUE), $data);
+            $this->session->set_flashdata('message', 'Update Record Success');
+            redirect(site_url('categorie_materiel'));
+        }
+    }
+    
+    public function delete($id) 
+    {
+        $row = $this->Categorie_materiel_model->get_by_id($id);
+
+        if ($row) {
+            $this->Categorie_materiel_model->delete($id);
+            $this->session->set_flashdata('message', 'Delete Record Success');
+            redirect(site_url('categorie_materiel'));
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('categorie_materiel'));
+        }
+    }
+
+    public function _rules() 
+    {
+	$this->form_validation->set_rules('nom_cat_mat', 'nom cat mat', 'trim|required');
+	$this->form_validation->set_rules('date_creation_cat', 'date creation cat', 'trim|required');
+	$this->form_validation->set_rules('details_cat_ma', 'details cat ma', 'trim|required');
+	$this->form_validation->set_rules('etat_cat_mat', 'etat cat mat', 'trim|required');
+
+	$this->form_validation->set_rules('id_cat_mat', 'id_cat_mat', 'trim');
+	$this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+    }
+
+    public function excel()
+    {
+        $this->load->helper('exportexcel');
+        $namaFile = "categorie_materiel.xls";
+        $judul = "categorie_materiel";
+        $tablehead = 0;
+        $tablebody = 1;
+        $nourut = 1;
+        //penulisan header
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+        header("Content-Disposition: attachment;filename=" . $namaFile . "");
+        header("Content-Transfer-Encoding: binary ");
+
+        xlsBOF();
+
+        $kolomhead = 0;
+        xlsWriteLabel($tablehead, $kolomhead++, "No");
+	xlsWriteLabel($tablehead, $kolomhead++, "Nom Cat Mat");
+	xlsWriteLabel($tablehead, $kolomhead++, "Date Creation Cat");
+	xlsWriteLabel($tablehead, $kolomhead++, "Details Cat Ma");
+	xlsWriteLabel($tablehead, $kolomhead++, "Etat Cat Mat");
+
+	foreach ($this->Categorie_materiel_model->get_all() as $data) {
+            $kolombody = 0;
+
+            //ubah xlsWriteLabel menjadi xlsWriteNumber untuk kolom numeric
+            xlsWriteNumber($tablebody, $kolombody++, $nourut);
+	    xlsWriteLabel($tablebody, $kolombody++, $data->nom_cat_mat);
+	    xlsWriteLabel($tablebody, $kolombody++, $data->date_creation_cat);
+	    xlsWriteLabel($tablebody, $kolombody++, $data->details_cat_ma);
+	    xlsWriteLabel($tablebody, $kolombody++, $data->etat_cat_mat);
+
+	    $tablebody++;
+            $nourut++;
+        }
+
+        xlsEOF();
+        exit();
+    }
+
+}
+
+/* End of file Categorie_materiel.php */
+/* Location: ./application/controllers/Categorie_materiel.php */
