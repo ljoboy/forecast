08c27eec824d50c7882da34aad871a347ac8c561
Jonathan Yombo <jonathanyombo@gmail.com>
2019-09-15 23:32:04 +0200

    [UPDATE] add agrnt's info to the session after login successfully
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -24,19 +24,25 @@ class Auth extends CI_Controller {
             redirect();
 
         $this->load->library('form_validation');
+        $this->load->model('agent_model');
 
         $this->form_validation->set_rules('username', "nom d'utilisateur", 'trim|required', ['required' => 'Le %s est obligatoire']);
         $this->form_validation->set_rules('password', 'mot de passe', 'required', ['required' => 'Le %s est obligatoire']);
 
         if ($this->form_validation->run() == TRUE) {
+
             $username = $this->input->post('username', true);
             $password = sha1($this->input->post('password'));
             $user = $this->auth_model->login($username, $password);
+
             if ($user != null) {
+                $agent = (array) $this->agent_model->get_by_id($user->agent_id_agent);
                 $array = array(
-                    'id' => $user->id,
+                    'id' => $user->id_user,
                     'username' => $user->username,
-                    'is_connected' => true
+                    'creer_le' => $user->create_time,
+                    'is_connected' => true,
+                    'info' => $agent
                 );
 
                 $this->session->set_userdata($array);
