1ed5f2a93ca1b9edd6ac36aaa05a8d5ae461e0c1
Jonathan Yombo <jonathanyombo@gmail.com>
2019-09-23 01:37:12 +0200

    [UPDATE] agent views
    Signed-off-by: Jonathan Yombo <jonathanyombo@gmail.com>

---
+++ application/views/agent/agent_form.php
@@ -0,0 +1,79 @@
+
+        <h2 style="margin-top:0px">Agent <?php echo $button ?></h2>
+        <form action="<?php echo $action; ?>" method="post">
+	    <div class="form-group">
+            <label for="varchar">Nom <?php echo form_error('nom') ?></label>
+            <input type="text" class="form-control" name="nom" id="nom" placeholder="Nom" value="<?php echo $nom; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Postnom <?php echo form_error('postnom') ?></label>
+            <input type="text" class="form-control" name="postnom" id="postnom" placeholder="Postnom" value="<?php echo $postnom; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Prenom <?php echo form_error('prenom') ?></label>
+            <input type="text" class="form-control" name="prenom" id="prenom" placeholder="Prenom" value="<?php echo $prenom; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Etat Civil <?php echo form_error('etat_civil') ?></label>
+            <input type="text" class="form-control" name="etat_civil" id="etat_civil" placeholder="Etat Civil" value="<?php echo $etat_civil; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Matricule <?php echo form_error('matricule') ?></label>
+            <input type="text" class="form-control" name="matricule" id="matricule" placeholder="Matricule" value="<?php echo $matricule; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Adresse <?php echo form_error('adresse') ?></label>
+            <input type="text" class="form-control" name="adresse" id="adresse" placeholder="Adresse" value="<?php echo $adresse; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Email <?php echo form_error('email') ?></label>
+            <input type="text" class="form-control" name="email" id="email" placeholder="Email" value="<?php echo $email; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="date">Date De Naissance <?php echo form_error('date_de_naissance') ?></label>
+            <input type="text" class="form-control" name="date_de_naissance" id="date_de_naissance" placeholder="Date De Naissance" value="<?php echo $date_de_naissance; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Lieu De Naissance <?php echo form_error('lieu_de_naissance') ?></label>
+            <input type="text" class="form-control" name="lieu_de_naissance" id="lieu_de_naissance" placeholder="Lieu De Naissance" value="<?php echo $lieu_de_naissance; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="double">Telephone <?php echo form_error('telephone') ?></label>
+            <input type="text" class="form-control" name="telephone" id="telephone" placeholder="Telephone" value="<?php echo $telephone; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="enum">Genre <?php echo form_error('genre') ?></label>
+            <input type="text" class="form-control" name="genre" id="genre" placeholder="Genre" value="<?php echo $genre; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="date">Date Entree <?php echo form_error('date_entree') ?></label>
+            <input type="text" class="form-control" name="date_entree" id="date_entree" placeholder="Date Entree" value="<?php echo $date_entree; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="date">Date Confirmation <?php echo form_error('date_confirmation') ?></label>
+            <input type="text" class="form-control" name="date_confirmation" id="date_confirmation" placeholder="Date Confirmation" value="<?php echo $date_confirmation; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="date">Date Fin <?php echo form_error('date_fin') ?></label>
+            <input type="text" class="form-control" name="date_fin" id="date_fin" placeholder="Date Fin" value="<?php echo $date_fin; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Ville <?php echo form_error('ville') ?></label>
+            <input type="text" class="form-control" name="ville" id="ville" placeholder="Ville" value="<?php echo $ville; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Province <?php echo form_error('province') ?></label>
+            <input type="text" class="form-control" name="province" id="province" placeholder="Province" value="<?php echo $province; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Pays <?php echo form_error('pays') ?></label>
+            <input type="text" class="form-control" name="pays" id="pays" placeholder="Pays" value="<?php echo $pays; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="int">Departement Id Departement <?php echo form_error('departement_id_departement') ?></label>
+            <input type="text" class="form-control" name="departement_id_departement" id="departement_id_departement" placeholder="Departement Id Departement" value="<?php echo $departement_id_departement; ?>" />
+        </div>
+	    <input type="hidden" name="id_agent" value="<?php echo $id_agent; ?>" /> 
+	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
+	    <a href="<?php echo site_url('agent') ?>" class="btn btn-default">Cancel</a>
+	</form>
---
+++ application/views/agent/agent_list.php
@@ -0,0 +1,76 @@
+
+        <div class="row" style="margin-bottom: 10px">
+            <div class="col-md-4">
+                <?php echo anchor(site_url('agent/create'),'Ajouter', 'class="btn btn-primary"'); ?>
+            </div>
+            <div class="col-md-4 text-center">
+                <div id="message">
+                    <?php echo $this->session->userdata('message') <> '' ? $this->session->userdata('message') : ''; ?>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <?php echo anchor(site_url('agent/excel'), 'Excel', 'class="btn btn-primary" style="float: right"'); ?>
+            </div>
+        </div>
+        <hr/>
+        <table id="datatable-buttons" class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nom</th>
+                    <th>Postnom</th>
+                    <th>Prenom</th>
+                    <th>Etat Civil</th>
+                    <th>Matricule</th>
+                    <th>Adresse</th>
+                    <th>Email</th>
+                    <th>Date De Naissance</th>
+                    <th>Lieu De Naissance</th>
+                    <th>Telephone</th>
+                    <th>Genre</th>
+                    <th>Date Entree</th>
+                    <th>Date Confirmation</th>
+                    <th>Ville</th>
+                    <th>Province</th>
+                    <th>Pays</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($agent_data as $agent)
+                {
+                    ?>
+                    <tr>
+                        <td width="80px"><?php echo ++$start ?></td>
+                        <td><?php echo $agent->nom ?></td>
+                        <td><?php echo $agent->postnom ?></td>
+                        <td><?php echo $agent->prenom ?></td>
+                        <td><?php echo $agent->etat_civil ?></td>
+                        <td><?php echo $agent->matricule ?></td>
+                        <td><?php echo $agent->adresse ?></td>
+                        <td><?php echo $agent->email ?></td>
+                        <td><?php echo nice_date($agent->date_de_naissance, 'd-m-Y') ?></td>
+                        <td><?php echo $agent->lieu_de_naissance ?></td>
+                        <td><?php echo $agent->telephone ?></td>
+                        <td><?php echo $agent->genre ?></td>
+                        <td><?php echo nice_date($agent->date_entree, 'd-m-Y') ?></td>
+                        <td><?php echo nice_date($agent->date_confirmation, 'd-m-Y') ?></td>
+                        <td><?php echo $agent->ville ?></td>
+                        <td><?php echo $agent->province ?></td>
+                        <td><?php echo $agent->pays ?></td>
+                        <td>
+                            <?php
+                            echo anchor(site_url('agent/read/'.$agent->id_agent),'Profil');
+                            echo ' | ';
+                            echo anchor(site_url('agent/update/'.$agent->id_agent),'Modifier');
+                            echo ' | ';
+                            echo anchor(site_url('agent/delete/'.$agent->id_agent),'Effacer','onclick="javascript: return confirm(\'Etes-vous sure ?\')"');
+                            ?>
+                        </td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </tbody>
+        </table>
